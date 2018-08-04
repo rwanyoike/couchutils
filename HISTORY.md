66314ebe72a61257dc27834cdbadd5a4684fbb69
Raymond Wanyoike <raymond.wanyoike@gmail.com>
2018-08-05 00:17:18 +0300

    Bump version: 0.1.0 → 0.2.0

--- HISTORY.md
+++ HISTORY.md
@@ -1,5 +1,9 @@
 # Version History
 
+## 0.2.0 (2018-08-05)
+
+- Add Python 3.7 support.
+
 ## 0.1.0 (2018-03-24)
 
 - First release on PyPI.
--- couchutils/__version__.py
+++ couchutils/__version__.py
@@ -1,7 +1,7 @@
 __title__ = 'couchutils'  # noqa: D100
 __description__ = 'A collection of CouchDB utils.'
 __url__ = 'https://github.com/rwanyoike/couchutils'
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 __author__ = 'Raymond Wanyoike'
 __author_email__ = 'raymond.wanyoike@gmail.com'
 __license__ = 'MIT'
